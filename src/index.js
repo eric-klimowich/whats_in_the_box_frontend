27dfa47fe6b739c8682e1021f26d49f05e8d7828
eric-klimowich <eklimowich@hotmail.com>
2019-04-10 15:03:00 -0400

    Fetch all boxes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
+//************************* Global Variables **************************//
+const boxesUrl = "http://localhost:3000/api/v1/boxes";
+let allBoxesArray = []
+//********************** End of Global Variables **********************//
+
+
+//******************** Listen for DOMContentLoaded ********************//
 document.addEventListener('DOMContentLoaded', function() {
 
+//**************** Variables Local to DOMContentLoaded ****************//
   const newBoxFormContainer = document.querySelector('#new-box-form-container')
+  const boxesContainer = document.querySelector('#boxes-container')
+//************* End of Variables Local to DOMContentLoaded ************//
 
+
+//************************** Fetch All Boxes **************************//
+  fetch(boxesUrl)
+    .then(r => r.json())
+    .then(allBoxes => {
+      allBoxesArray = allBoxes
+      console.log(allBoxesArray)
+    })
+//*********************** End of Fetch All Boxes **********************//
+
+console.log(allBoxesArray)
+//*************************** New Box Form ****************************//
   newBoxFormContainer.innerHTML = `
     <form>
       <input type="text" placeholder="Box label..." />
@@ -11,5 +33,8 @@ document.addEventListener('DOMContentLoaded', function() {
       <input type="submit" value="Submit" />
     </form>
   `
+//************************ End of New Box Form ************************//
+
 
 })
+//***************** End of Listen for DOMContentLoaded ****************//
